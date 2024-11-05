3d9c2e910051833b603600ed6d3a8278f83a36b4
Abdul Hadi <143840294+abdulhadi08878@users.noreply.github.com>
2024-11-06 01:04:44 +0500

    afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Change directory to the application folder
 cd /home/ubuntu
-npm -f install
+
+# Ensure PM2 is installed
+npm install -g pm2
+
+# Start the application with PM2
+sudo pm2 -f start server.js
+
+# Optional: Ensure PM2 restarts app on server reboot
+pm2 startup systemd
+pm2 save
